467907d442a95409dbc3b06d2a90483ca9054057
László Vaskó <1771332+vlaci@users.noreply.github.com>
2024-07-26 14:10:50 +0200

    Enable passing buildInputs as well

--- buildMaturinPythonPackage.nix
+++ buildMaturinPythonPackage.nix
@@ -19,112 +19,122 @@
 let
   inherit (lib) filterAttrs isDerivation optional optionalAttrs optionalString;
   drv = lib.makeOverridable
-    (args@{ pname ? null, src, testSrc ? src, coverage ? false, python ? python3, ... }: python.pkgs.buildPythonPackage
-      (
-        let
-          commonArgs = {
-            inherit src;
+    (args@{ pname ? null
+     , src
+     , testSrc ? src
+     , buildInputs ? [ ]
+     , nativeBuildInputs ? [ ]
+     , coverage ? false
+     , python ? python3
+     , ...
+     }:
+      python.pkgs.buildPythonPackage
+        (
+          let
+            commonArgs = {
+              inherit src nativeBuildInputs;
 
-            doNotLinkInheritedArtifacts = true;
-            installCargoArtifactsMode = "use-zstd";
-            env.PYO3_PYTHON = "${python}/bin/python";
+              doNotLinkInheritedArtifacts = true;
+              installCargoArtifactsMode = "use-zstd";
+              env.PYO3_PYTHON = "${python}/bin/python";
 
-            buildInputs = [
-              python
-            ] ++ lib.optionals stdenv.isDarwin [
-              libiconv
-            ];
-          };
-
-          cargoVendorDir = craneLib.vendorCargoDeps { inherit src; };
-
-          cargoMaturinArtifacts = craneLib.buildDepsOnly ((builtins.removeAttrs commonArgs [ "src" ]) // {
-            pnameSuffix = "maturin-deps";
-            dummySrc = craneLib.mkDummySrc {
-              inherit src;
-              extraDummyScript =
-                let
-                  pyprojectToml = builtins.fromTOML (builtins.readFile (src + "/pyproject.toml"));
-                  cleanedPyprojectToml = {
-                    inherit (pyprojectToml) build-system;
-                    tool.maturin = pyprojectToml.tool.maturin // {
-                      python-source = "/nonexistent/but/its/okay";
-                    };
-                  };
-                in
-                ''
-                  cp ${craneLib.writeTOML "pyproject.toml" cleanedPyprojectToml} $out/pyproject.toml
-                '';
+              buildInputs = [
+                python
+              ] ++ lib.optionals stdenv.isDarwin [
+                libiconv
+              ] ++ buildInputs;
             };
 
-            buildPhaseCargoCommand = ''
-              ${rust.envVars.setEnv} maturin build --manylinux off --release
-              rm -rf target/wheels
-            '';
-            doCheck = false;
-            cargoToml = src + "/Cargo.toml";
+            cargoVendorDir = craneLib.vendorCargoDeps { inherit src; };
+
+            cargoMaturinArtifacts = craneLib.buildDepsOnly ((builtins.removeAttrs commonArgs [ "src" ]) // {
+              pnameSuffix = "maturin-deps";
+              dummySrc = craneLib.mkDummySrc {
+                inherit src;
+                extraDummyScript =
+                  let
+                    pyprojectToml = builtins.fromTOML (builtins.readFile (src + "/pyproject.toml"));
+                    cleanedPyprojectToml = {
+                      inherit (pyprojectToml) build-system;
+                      tool.maturin = pyprojectToml.tool.maturin // {
+                        python-source = "/nonexistent/but/its/okay";
+                      };
+                    };
+                  in
+                  ''
+                    cp ${craneLib.writeTOML "pyproject.toml" cleanedPyprojectToml} $out/pyproject.toml
+                  '';
+              };
+
+              buildPhaseCargoCommand = ''
+                ${rust.envVars.setEnv} maturin build --manylinux off --release
+                rm -rf target/wheels
+              '';
+              doCheck = false;
+              cargoToml = src + "/Cargo.toml";
+              inherit cargoVendorDir;
+              nativeBuildInputs = [ maturin python rustc ];
+            });
+
+            cargoArtifacts = (craneLib.buildDepsOnly (commonArgs // {
+              inherit cargoVendorDir;
+            })).overrideAttrs (_: { cargoArtifacts = cargoMaturinArtifacts; });
+
+            crate = craneLib.buildPackage (commonArgs // {
+              inherit cargoArtifacts;
+            });
+
+            rustPlatform = makeRustPlatform { inherit cargo rustc; };
+
+          in
+          (lib.recursiveUpdate (commonArgs // (builtins.removeAttrs args [ "cargo" "rustc" "coverage" ])) {
+            inherit (crate) version;
+            pname = (if pname != null then pname else crate.pname) + (optionalString coverage "-coverage");
+
             inherit cargoVendorDir;
-            nativeBuildInputs = [ maturin python rustc ];
-          });
+            cargoArtifacts = cargoMaturinArtifacts;
+            format = "pyproject";
 
-          cargoArtifacts = (craneLib.buildDepsOnly (commonArgs // {
-            inherit cargoVendorDir;
-          })).overrideAttrs (_: { cargoArtifacts = cargoMaturinArtifacts; });
+            strictDeps = true;
 
-          crate = craneLib.buildPackage (commonArgs // {
-            inherit cargoArtifacts;
-          });
+            preConfigure = optionalString coverage ''
+              source <(cargo llvm-cov show-env --export-prefix)
+            '' + (args.preConfigure or "");
 
-          rustPlatform = makeRustPlatform { inherit cargo rustc; };
+            env = optionalAttrs coverage {
+              LLVM_COV = "${llvm}/bin/llvm-cov";
+              LLVM_PROFDATA = "${llvm}/bin/llvm-profdata";
+            };
 
-        in
-        (lib.recursiveUpdate (commonArgs // (builtins.removeAttrs args [ "cargo" "rustc" "coverage" ])) {
-          inherit (crate) version;
-          pname = (if pname != null then pname else crate.pname) + (optionalString coverage "-coverage");
+            nativeBuildInputs =
+              let
+                rustHooks = callPackage "${nixpkgs}/pkgs/build-support/rust/hooks" { };
+              in
+              with rustPlatform; with craneLib; [
+                cargo
+                cargoHelperFunctionsHook
+                configureCargoCommonVarsHook
+                configureCargoVendoredDepsHook
+                inheritCargoArtifactsHook
+                installCargoArtifactsHook
+                replaceCargoLockHook
+                rsync
+                zstd
+                (rustHooks.maturinBuildHook.override { pkgsHostTarget = { inherit maturin cargo rustc; }; })
+              ]
+              ++ nativeBuildInputs
+              ++ optional coverage cargo-llvm-cov;
 
-          inherit cargoVendorDir;
-          cargoArtifacts = cargoMaturinArtifacts;
-          format = "pyproject";
-
-          strictDeps = true;
-
-          preConfigure = optionalString coverage ''
-            source <(cargo llvm-cov show-env --export-prefix)
-          '' + (args.preConfigure or "");
-
-          env = optionalAttrs coverage {
-            LLVM_COV = "${llvm}/bin/llvm-cov";
-            LLVM_PROFDATA = "${llvm}/bin/llvm-profdata";
-          };
-
-          nativeBuildInputs =
-            let
-              rustHooks = callPackage "${nixpkgs}/pkgs/build-support/rust/hooks" { };
-            in
-            with rustPlatform; with craneLib; [
-              cargo
-              cargoHelperFunctionsHook
-              configureCargoCommonVarsHook
-              configureCargoVendoredDepsHook
-              inheritCargoArtifactsHook
-              installCargoArtifactsHook
-              replaceCargoLockHook
-              rsync
-              zstd
-              (rustHooks.maturinBuildHook.override { pkgsHostTarget = { inherit maturin cargo rustc; }; })
-            ]
-            ++ (args.nativeBuildInputs or [ ])
-            ++ optional coverage cargo-llvm-cov;
-
-          passthru = {
-            tests = filterAttrs
-              (n: v: isDerivation v)
-              (callPackage ./crane-maturin-tests.nix { inherit craneLib commonArgs cargoArtifacts crate drv cargo testSrc python; })
-            ;
-            withCoverage = drv.override { coverage = true; };
-          };
-        })
-      ))
+            passthru = {
+              inherit crate;
+              tests = filterAttrs
+                (n: v: isDerivation v)
+                (callPackage ./crane-maturin-tests.nix { inherit craneLib commonArgs cargoArtifacts crate drv cargo testSrc python; })
+              ;
+              withCoverage = drv.override { coverage = true; };
+            };
+          })
+        ))
     args;
 in
 drv
